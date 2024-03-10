9688515ebddaf4623ebee26b76829ae1410f67cc
Aurnab <zubayeraurnab@gmail.com>
2024-03-10 13:22:50 +0600

    Payload added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment,incrementByValue } from "../redux/features/counter/counterSlice";
 
 
 const Counter = () => {
+    const { count } = useSelector((state) => state.counter)
+    const dispatch = useDispatch()
     return (
         <div>
-            
+            <button className="p-4 bg-black text-white rounded-full" onClick={() => dispatch(increment())}>increment</button>
+            <button className="p-4 bg-black text-white rounded-full" onClick={() => dispatch(incrementByValue(5))}>increment By 5</button>
+            <div>
+                <h1 className="text-3xl py-4">{count}</h1>
+            </div>
+            <button className="p-4 bg-black text-white rounded-full" onClick={() => dispatch(decrement())}>decrement</button>
         </div>
     );
 };
